99c61f835b42b02eeea52ef4d381b7f92dd6818c
Steve Kinney <hello@stevekinney.net>
2012-07-07 09:55:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Three Boxes: A jQuery Lab
+
+This little project is a hands-on exercise for getting comfortable with jQuery. More details to follow.
---
+++ scripts/scriptery-min.js
@@ -0,0 +1,1 @@
+var $bluebox,$greenbox,$redbox,$button1,$button2,$button3;$redbox=$("#red");$bluebox=$("#blue");$greenbox=$("#green");$button1=$("#button1");$button2=$("#button2");$button3=$("#button3");
