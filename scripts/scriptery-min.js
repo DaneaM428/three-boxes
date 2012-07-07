ab2173f0012e93f848e252720b919a789cd4d43a
Steve Kinney <hello@stevekinney.net>
2012-07-07 11:18:49 -0400

    Change 'scriptery.js' to 'your-scripts.js'

--- scripts/scriptery-min.js
+++
@@ -1,1 +0,0 @@
-var $bluebox,$greenbox,$redbox,$button1,$button2,$button3;$redbox=$("#red");$bluebox=$("#blue");$greenbox=$("#green");$button1=$("#button1");$button2=$("#button2");$button3=$("#button3");
